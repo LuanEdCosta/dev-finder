c9c1a6f2a37198d5fa0b776776c49170fe5897b8
Luan Eduardo da Costa <luan1346@gmail.com>
2020-02-01 15:43:35 -0300

    Solving routing problems

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,24 +1,27 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Switch, Route, HashRouter } from 'react-router-dom'
 import Home from '../Home'
 import SearchResult from '../SearchResult'
 import PageNotFound from '../PageNotFound'
 
+// The process.env.PUBLIC_URL will be empty in development
+// and dev-finder in production
+
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/search/:user" exact>
+        <Route path="/search/:user">
           <SearchResult />
         </Route>
         <Route path="*">
           <PageNotFound />
         </Route>
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
