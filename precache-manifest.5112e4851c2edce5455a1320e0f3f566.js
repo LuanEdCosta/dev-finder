0f4e96b0a2c55a81179c3057ce3d62e96bd96df2
Luan Eduardo da Costa <luan1346@gmail.com>
2020-02-01 15:32:55 -0300

    Updates

--- precache-manifest.5112e4851c2edce5455a1320e0f3f566.js
+++ precache-manifest.5112e4851c2edce5455a1320e0f3f566.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dd476d62ad4be283553890ad306a895e",
+    "revision": "b843b9a8c66189e60916ec700bdecc00",
     "url": "/dev-finder/index.html"
   },
   {
-    "revision": "cc43efda37f0df8f1e5c",
+    "revision": "d7e6be771a41eace5844",
     "url": "/dev-finder/static/css/main.4fe9c0dd.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/dev-finder/static/js/2.0222082c.chunk.js.LICENSE"
   },
   {
-    "revision": "cc43efda37f0df8f1e5c",
-    "url": "/dev-finder/static/js/main.796e39bc.chunk.js"
+    "revision": "d7e6be771a41eace5844",
+    "url": "/dev-finder/static/js/main.a767a159.chunk.js"
   },
   {
     "revision": "820325d4f5c2f43f4a11",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dev-finder/precache-manifest.5b80ba94d39a7c03cf88dffdbdfe8726.js"
+  "/dev-finder/precache-manifest.5112e4851c2edce5455a1320e0f3f566.js"
 );
 
 self.addEventListener('message', (event) => {
